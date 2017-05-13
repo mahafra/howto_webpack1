f72cd44b4d3b7f43d401471ef344e8eccaeeddb5
mahafra <post@hafranke.de>
2017-05-13 23:20:17 +0200

    step2

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@
      output: {
          path:  __dirname + '/dist',
          filename: 'bundle.js'
-     }
+     },
+	 module: {
+		 rules: [
+			{ 
+				test: /\.jsx?$/, 
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				options: {
+					plugins: ['transform-runtime'],
+					presets: ['es2015']
+				}
+			}
+		 ]
+	 }		 
  };
