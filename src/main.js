5cafca18ca0b483692acf7e249b4a1603b3bc82e
mahafra <post@hafranke.de>
2017-05-14 10:09:40 +0200

    step 5 Lazy Loading Chunks

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { map } from 'lodash';
-import template from './numberlist.hbs';
+// import template from './numberlist.hbs';
 
 let numbers = map([1,2,3,4,5,6], n => n*n);
 
-document.getElementById('app-container').innerHTML = template({numbers});
+
+
+
+setTimeout( () => {
+	require(['./numberlist.hbs'], template => {
+		document.getElementById('app-container').innerHTML = template({numbers});
+	})
+}, 4000);
